a0fae6d20787d7c125410bf3b42442cc9918b916
Kurt Dorflinger <kurtd5105@gmail.com>
2018-10-29 14:08:19 -0700

    Make travis tests only use the NonGUITests and add basic test to the test class.

--- src/test/java/ca/uvic/seng330/assn3/NonGUITest.java
+++ src/test/java/ca/uvic/seng330/assn3/NonGUITest.java
@@ -3,6 +3,11 @@ package ca.uvic.seng330.assn3;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
-public class NonGUITest {
+import org.junit.Test;
 
+public class NonGUITest {
+    @Test
+    public void ApplicationExists() {
+        ClickApplication c = new ClickApplication();
+    }
 }
