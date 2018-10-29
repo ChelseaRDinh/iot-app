90845476cbc20d5fb344b370bc494cb392d9d157
Kurt Dorflinger <kurtd5105@gmail.com>
2018-10-29 14:47:20 -0700

    Update non gui tests to follow code formatting.

--- src/test/java/ca/uvic/seng330/assn3/NonGUITest.java
+++ src/test/java/ca/uvic/seng330/assn3/NonGUITest.java
@@ -1,13 +1,10 @@
 package ca.uvic.seng330.assn3;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-
 import org.junit.Test;
 
 public class NonGUITest {
-    @Test
-    public void ApplicationExists() {
-        ClickApplication c = new ClickApplication();
-    }
+  @Test
+  public void ApplicationExists() {
+    ClickApplication c = new ClickApplication();
+  }
 }
