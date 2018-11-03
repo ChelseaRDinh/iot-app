276316bd7d25efe396bcf2cc06ed7f9325b9d8c2
ChelseaRDinh <chelseadinh94@gmail.com>
2018-11-03 16:58:34 -0700

    Include all files in home package.

--- src/main/java/ca/uvic/seng330/assn3/home/HomeView.java
+++ src/main/java/ca/uvic/seng330/assn3/home/HomeView.java
@@ -6,12 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 
 public class HomeView {
-    public HomeView() {
+    private GridPane view;
+    private HomeController controller;
+    private HomeModel model;
+
+    public HomeView(HomeController controller, HomeModel model) {
+        this.controller = controller;
+        this.model = model;
     }
 }
